cb56cfe438171d44e0b72c4727763f9f008bad0e
Pendulun <danielcampos14@hotmail.com.br>
2021-02-06 20:17:04 -0300

    Classe Trie

--- include/compressor/Trie.hpp
+++ include/compressor/Trie.hpp
@@ -1,4 +1,21 @@
 #ifndef TRIE_H
 #define TRIE_H
+#include <iostream>
+#include <string>
+#include "Node.hpp"
+namespace compressor{
+	class Trie{
+	public:
+		Node* getRaiz();
+		virtual void inserirNo(Node* filho,Node* pai) = 0;
+		virtual Node* pesquisar(std::string padrao) = 0;
+		~Trie() virtual;
+		Trie();
+	private:
+		Node* raiz;
+	protected:
+		void setRaiz(Node* raiz);
+	};
+}
 
 #endif
--- src/compressor/Trie.cpp
+++ src/compressor/Trie.cpp
@@ -1,1 +1,24 @@
 #include "Trie.hpp"
+
+namespace compressor{
+	
+	Node* Trie::getRaiz(){
+		return this->raiz;
+	}
+
+	void Trie::setRaiz(Node* novaRaiz){
+		this->raiz = novaRaiz;
+	}
+
+	void Trie::inserirNo(Node* filho,Node* pai){}
+
+	Node* Trie::pesquisar(std::string padrao){}
+
+	Trie::Trie(){}
+
+	Trie::~Trie(){
+		std::cout<<"Destrutor Trie"<<std::endl;
+		std::cout<<"Deletando raiz"<<std::endl;
+		delete this->raiz;
+	}
+}
